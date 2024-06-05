715ec7a7d7745f5cb1dde670b9b1508322ff97a3
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-05 15:18:08 +0200

    Trying fix the 403 error

--- Server.js
+++ Server.js
@@ -1,23 +1,18 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const SpotifyWebApi = require("spotify-web-api-node");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 8888;
+
+app.use(cors({ origin: "https://workzen-webapp.onrender.com" }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    message: err.message,
-    error: process.env.NODE_ENV === "production" ? {} : err,
-  });
-});
-
-app.post("/refresh", (req, res, next) => {
+app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
@@ -25,32 +20,29 @@ app.post("/refresh", (req, res, next) => {
     clientSecret: process.env.CLIENT_SECRET,
     refreshToken,
   });
+
   spotifyApi
     .refreshAccessToken()
     .then((data) => {
-      res.json({
-        accessToken: data.body.access_token,
-        expiresIn: data.body.expires_in,
-      });
+      app.post("/refresh", (req, res) => {});
     })
     .catch((err) => {
-      err.status = 400;
-      next(err);
+      console.error(err);
+      res.sendStatus(400);
     });
 });
 
-app.post("/login", (req, res, next) => {
+app.post("/login", (req, res) => {
   const code = req.body.code;
-  console.log("Received code:", code);
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
   });
+
   spotifyApi
     .authorizationCodeGrant(code)
     .then((data) => {
-      console.log(code, data);
       res.json({
         accessToken: data.body.access_token,
         refreshToken: data.body.refresh_token,
@@ -58,39 +50,15 @@ app.post("/login", (req, res, next) => {
       });
     })
     .catch((err) => {
-      err.status = 400;
-      next(err);
-    });
-});
-
-app.get("/callback", (req, res, next) => {
-  const code = req.query.code || null;
-
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  });
-
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then((data) => {
-      const { access_token, refresh_token, expires_in } = data.body;
-
-      res.redirect(
-        `/?access_token=${access_token}&refresh_token=${refresh_token}&expires_in=${expires_in}`
-      );
-    })
-    .catch((err) => {
       console.error(err);
-      res.redirect("/?error=invalid_token");
+      res.sendStatus(400);
     });
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World, how are you!!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
