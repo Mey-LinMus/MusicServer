c491c494c8e49c04d4a9f8a23529830f5459f18f
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-05 11:21:46 +0200

    Bach tot the login code

--- Server.js
+++ Server.js
@@ -5,97 +5,92 @@ const bodyParser = require("body-parser");
 const SpotifyWebApi = require("spotify-web-api-node");
 const app = express();
 const port = process.env.PORT || 8888;
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: "https://workzen-webapp.onrender.com",
-};
-app.use(cors(corsOptions));
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: process.env.NODE_ENV === "production" ? {} : err,
+  });
 });
 
-let accessToken = "";
-
-spotifyApi.clientCredentialsGrant().then(
-  function (data) {
-    console.log("The access token expires in " + data.body["expires_in"]);
-    console.log("The access token is " + data.body["access_token"]);
-
-    accessToken = data.body["access_token"];
-
-    setInterval(() => {
-      spotifyApi.clientCredentialsGrant().then(
-        function (data) {
-          accessToken = data.body["access_token"];
-          console.log("The access token has been refreshed");
-        },
-        function (err) {
-          console.log("Could not refresh the access token", err);
-        }
-      );
-    }, (data.body["expires_in"] - 60) * 1000);
-  },
-  function (err) {
-    console.log("Something went wrong when retrieving an access token", err);
-  }
-);
-
-app.get("/api/albums/:id", (req, res) => {
-  const albumId = req.params.id;
-
-  spotifyApi.setAccessToken(accessToken);
+app.post("/refresh", (req, res, next) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  });
   spotifyApi
-    .getAlbum(albumId)
+    .refreshAccessToken()
     .then((data) => {
-      res.json(data.body);
-    })
-    .catch((err) => {
-      console.error("Error getting album:", err);
-      res.status(500).send("Internal Server Error");
-    });
-});
-
-app.get("/api/playlists/:id", (req, res) => {
-  const playlistId = req.params.id;
-
-  spotifyApi.setAccessToken(accessToken);
-  spotifyApi
-    .getPlaylistTracks(playlistId)
-    .then((data) => {
-      const tracks = data.body.items.map((item) => {
-        const track = item.track;
-        const smallestAlbumImage = track.album.images.reduce(
-          (smallest, image) => {
-            if (image.height < smallest.height) return image;
-            return smallest;
-          },
-          track.album.images[0]
-        );
-        return {
-          artist: track.artists[0].name,
-          title:
-            track.name.length > 25
-              ? track.name.substring(0, 25) + "..."
-              : track.name,
-          uri: track.uri,
-          albumUrl: smallestAlbumImage.url,
-          duration_ms: track.duration_ms,
-        };
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
       });
-      res.json(tracks);
     })
     .catch((err) => {
-      console.error("Error fetching playlist tracks:", err);
-      res.status(500).send("Internal Server Error");
+      err.status = 400;
+      next(err);
     });
+});
+
+app.post("/login", (req, res, next) => {
+  const code = req.body.code;
+  console.log("Received code:", code);
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      console.log(code, data);
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      err.status = 400;
+      next(err);
+    });
+});
+
+app.get("/callback", (req, res, next) => {
+  const code = req.query.code || null;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      const { access_token, refresh_token, expires_in } = data.body;
+
+      res.redirect(
+        `/?access_token=${access_token}&refresh_token=${refresh_token}&expires_in=${expires_in}`
+      );
+    })
+    .catch((err) => {
+      console.error(err);
+      res.redirect("/?error=invalid_token");
+    });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
