f47dde824a68bf52507dbcb73b6576268eb6dc97
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-05 09:35:46 +0200

    Updating server so the user doesn't need to login to spotify

--- Server.js
+++ Server.js
@@ -10,89 +10,55 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    message: err.message,
-    error: process.env.NODE_ENV === "production" ? {} : err,
-  });
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
 });
 
-app.post("/refresh", (req, res, next) => {
-  const refreshToken = req.body.refreshToken;
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken,
-  });
-  spotifyApi
-    .refreshAccessToken()
-    .then((data) => {
-      res.json({
-        accessToken: data.body.access_token,
-        expiresIn: data.body.expires_in,
-      });
-    })
-    .catch((err) => {
-      err.status = 400;
-      next(err); 
-    });
-});
 
-app.post("/login", (req, res, next) => {
-  const code = req.body.code;
-  console.log("Received code:", code);
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  });
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then((data) => {
-      console.log(code, data);
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      });
-    })
-    .catch((err) => {
-      err.status = 400;
-      next(err); 
-    });
-});
+let accessToken = "";
 
-app.get("/callback", (req, res, next) => {
-  const code = req.query.code || null;
+spotifyApi.clientCredentialsGrant().then(
+  function (data) {
+    console.log("The access token expires in " + data.body["expires_in"]);
+    console.log("The access token is " + data.body["access_token"]);
 
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  });
 
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then((data) => {
-      const { access_token, refresh_token, expires_in } = data.body;
+    accessToken = data.body["access_token"];
 
-      res.redirect(
-        `/?access_token=${access_token}&refresh_token=${refresh_token}&expires_in=${expires_in}`
+
+    setInterval(() => {
+      spotifyApi.clientCredentialsGrant().then(
+        function (data) {
+          accessToken = data.body["access_token"];
+          console.log("The access token has been refreshed");
+        },
+        function (err) {
+          console.log("Could not refresh the access token", err);
+        }
       );
+    }, (data.body["expires_in"] - 60) * 1000); 
+  },
+  function (err) {
+    console.log("Something went wrong when retrieving an access token", err);
+  }
+);
+
+app.get("/api/albums/:id", (req, res) => {
+  const albumId = req.params.id;
+
+  spotifyApi.setAccessToken(accessToken);
+  spotifyApi
+    .getAlbum(albumId)
+    .then((data) => {
+      res.json(data.body);
     })
     .catch((err) => {
-      console.error(err);
-      res.redirect("/?error=invalid_token");
+      console.error("Error getting album:", err);
+      res.status(500).send("Internal Server Error");
     });
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
